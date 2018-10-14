4c1407ceb4818094754a75b878f2634b6706f854
lilei-ncut/netcoredemo.git <baqiaoll007@163.com>
2018-10-14 16:34:18 +0800

    create

---
+++ src/GRPCDemo/RouteGuide/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RouteGuide
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/GRPCDemo/RouteGuideServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RouteGuideServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
