b78340d31ca0cc27c4dfb4956e88d02c891f1230
lilei <lilei@laplacetech.cn>
2018-10-17 16:58:40 +0800

    测试grpc 调用

--- src/GRPCDemo/RouteGuide/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RouteGuide
-{
-    public class Class1
-    {
-    }
-}
--- src/GRPCDemo/RouteGuideServer/Program.cs
+++ src/GRPCDemo/RouteGuideServer/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Grpc.Core;
+using RouteGuide;
 
-namespace RouteGuideServer
+namespace Routeguide
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            const int Port = 50052;
+
+            var features = RouteGuideUtil.ParseFeatures(RouteGuideUtil.DefaultFeaturesFile);
+            Server server = new Server
+            {
+                Services = { RouteGuide.BindService(new RouteGuideImpl(features)) },
+                Ports = { new ServerPort("localhost",Port,ServerCredentials.Insecure)}
+            };
+            server.Start();
+            Console.WriteLine("RouteGuide server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
         }
     }
 }
